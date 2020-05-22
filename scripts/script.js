1fb285ce929c331aabe00744991f5c40572d91ac
Yu Hao Wong <wongyuhao@gmail.com>
2020-05-22 20:51:39 +0800

    testing mobile site detection

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,9 @@
 window.onload=function(){
+
+    if (screen.width <= 699) {
+        document.location = "mobile.html";
+    }
+
     const image = document.querySelector('#main-image');
 
     image.addEventListener('mouseenter',function(){
