72f0dafd82a8f6dfcfffc719a1dcabcad10fb17b
Yu Hao Wong <wongyuhao@gmail.com>
2020-06-03 18:55:02 +0800

    restructerd pages

---
+++ scripts/redirect.js
@@ -0,0 +1,4 @@
+window.onload=function(){
+    this.document.location = "awareness.html"
+    
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,4 @@
 window.onload=function(){
-    this.document.location = "awareness.html"
-    
-
 
     const image = document.querySelector('#main-image');
 
@@ -16,6 +13,4 @@ window.onload=function(){
     image.addEventListener('click',function(){
         window.location = "awareness.html";
     })
-
-
-}
+  }
